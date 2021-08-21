fc534c30bbd322617c490e041c04171e6385a523
WhiteScoupeBr <jpcosta_severo@hotmail.com>
2021-08-21 15:40:01 -0300

    test

--- ami.py
+++ ami.py
@@ -7,7 +7,7 @@ voltageLevel = 5 # peak voltage level in Volts
 data = (np.random.rand(10000)>0.5).astype(int) # random 1s and 0s for data
 clk = np.arange(0,2*len(data)) % 2 # clock samples
 
-
+#teste
 ami = 1*data; previousOne = 0 
 
 for ii in range(0,len(data)):
